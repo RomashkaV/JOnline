32b0949821ee7bf2b97b8bcdec616e057bae981f
RomashkaV <85069973+RomashkaV@users.noreply.github.com>
2021-08-09 16:48:21 +0300

    Add files via upload

---
+++ BasicTask1.java
@@ -0,0 +1,18 @@
+package by.jonline.basic.main;
+
+//Найти  значение функции: z = ( (a – 3 ) * b / 2) + c.
+
+public class BasicTask1 {
+	public static void main(String[] args) {
+		
+		int a = 0;
+		int b = 3;
+		int c = 5;
+		double result;
+		
+		result = ((a - 3) * b / 2.0 ) + c;
+		
+		System.out.println(result);
+	}
+
+}
